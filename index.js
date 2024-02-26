17923789dfd5b3aea3614eaa40c84f4f47cf5bc6
RamasiaSerero <sereroramasia05@gmail.com>
2024-02-26 12:05:26 +0200

    fixed padding nut form still not responsive

--- index.js
+++
@@ -1,8 +0,0 @@
-const navLinks = document.querySelectorAll(".nav-link");
-const menuToggle = document.getElementById("navbarSupportedContent");
-const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false });
-navLinks.forEach((l) => {
-  l.addEventListener("click", () => {
-    bsCollapse.toggle();
-  });
-});
